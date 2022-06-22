27b65173e211cced00a315bf5e4cc7d515295673
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-06-22 11:04:44 +0000

    modified request headers

--- scraper-files/milk/spiders/sg-ov8.py
+++ scraper-files/milk/spiders/sg-ov8.py
@@ -88,22 +88,22 @@ class SgOv8Spider(scrapy.Spider):
 		]
 
 		headers = {
-		'authority': 'shengsiong.com.sg',
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-		'accept-language': 'en-GB,en;q=0.9',
-		'cache-control': 'max-age=0',
-		'dnt': '1',
-		'referer': 'https://shengsiong.com.sg',
-		'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-		'sec-ch-ua-mobile': '?0',
-		'sec-ch-ua-platform': '"macOS"',
-		'sec-fetch-dest': 'document',
-		'sec-fetch-mode': 'navigate',
-		'sec-fetch-site': 'none',
-		'sec-fetch-user': '?1',
-		'upgrade-insecure-requests': '1',
-		'user-agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.
-		36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36''',
+			'authority': 'shengsiong.com.sg',
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+			'accept-language': 'en-GB,en;q=0.9',
+			'cache-control': 'max-age=0',
+			'dnt': '1',
+			'referer': 'https://shengsiong.com.sg',
+			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"macOS"',
+			'sec-fetch-dest': 'document',
+			'sec-fetch-mode': 'navigate',
+			'sec-fetch-site': 'none',
+			'sec-fetch-user': '?1',
+			'upgrade-insecure-requests': '1',
+			'user-agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.
+			36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36''',
 		}
 		
 
@@ -130,8 +130,8 @@ class SgOv8Spider(scrapy.Spider):
 			
 	async def parse(self, response):
 		
-		page = response.meta["playwright_page"]
-		await page.close()
+		# page = response.meta["playwright_page"]
+		# await page.close()
 
 		products = response.css("div.card div.card-body")
 		product_list = []
