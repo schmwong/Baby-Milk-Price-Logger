d58eff14a21d0887d5d0fad9d4b94f49b23aacce
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-06-27 01:12:23 +0000

    .py script to test parse yaml file

--- reschedule_next_run.py
+++ reschedule_next_run.py
@@ -1,3 +1,31 @@
-import os
 
-print("File path of failed workflow: " , os.environ["workflow_path"])
+# This script only works within Github Actions
+# pip install pyyaml
+# ---
+
+import os
+import yaml
+
+
+workflow_file = os.environ["workflow_path"]
+
+with open(workflow_file) as file:
+	wf = yaml.safe_load(file)
+
+	print(
+		f'''
+
+
+
+		Steps if caller workflow fails:
+		{wf["jobs"]["on-failure"]["steps"]}
+
+		
+		
+		'''
+	)
+	
+	print("\n\nWhole workflow:\n", wf, "\n\n")
+
+
+print("\n\nFile path of failed workflow: " , os.environ["workflow_path"])
