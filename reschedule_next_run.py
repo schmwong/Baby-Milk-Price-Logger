b972103d3ecbb131281a302c5d2bc1d7d5e9510e
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-06-27 01:28:08 +0000

    .get method for key error

--- reschedule_next_run.py
+++ reschedule_next_run.py
@@ -9,23 +9,25 @@ import yaml
 
 workflow_file = os.environ["workflow_path"]
 
-with open(workflow_file) as file:
+with open(workflow_file, "r") as file:
 	wf = yaml.safe_load(file)
 
-	print(
-		f'''
-
-
-
-		Steps if caller workflow fails:
-		{wf["jobs"]["on-failure"]["steps"]}
-
-		
-		
-		'''
-	)
+	try:
+		print(
+			f'''
 	
-	print("\n\nWhole workflow:\n", wf, "\n\n")
+	
+	
+			Steps if caller workflow fails:
+			{wf["jobs"].get("on-failure").get("steps")}
+	
+			
+			
+			'''
+		)
+		
+	finally:
+		print("\n\nWhole workflow:\n", wf, "\n\n")
 
 
 print("\n\nFile path of failed workflow: " , os.environ["workflow_path"])
