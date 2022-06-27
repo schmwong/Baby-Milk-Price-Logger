af57fcad96f443d914a11aca72fb4aff821ddf25
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-06-27 06:19:13 +0000

    Test write

--- reschedule_next_run.py
+++ reschedule_next_run.py
@@ -9,31 +9,34 @@ import yaml
 
 workflow_file = os.environ["workflow_path"]
 
-with open(workflow_file, "r+") as file:
+with open(workflow_file, "r") as file:
 	wf = yaml.safe_load(file)
-	wf["jobs"]["scrape"]["runs-on"] = "ubuntu-latest"
-	wf = yaml.dump(wf, file)
-
-	try:
-		print(
-			f'''
-
-			
-			Scraper runs on {wf["jobs"]["scrape"]["runs-on"]}
-			Scraper uses Python {wf["jobs"]["scrape"]["steps"][1]["with"]["python-version"]}
 	
 	
-			Steps if caller workflow fails:
-			{wf["jobs"]["scrape"]["steps"]}
+wf["jobs"]["scrape"]["runs-on"] = "ubuntu-latest"
 
-			
-			'''
-		)
+
+with open(workflow_file, "w") as file:
+	yaml.safe_dump(wf, file, default_flow_style=False)
+	
+try:
+	print(
+		f'''
 
 		
+		Scraper runs on {wf["jobs"]["scrape"]["runs-on"]}
+		Scraper uses Python {wf["jobs"]["scrape"]["steps"][1]["with"]["python-version"]}
+
+
+		Steps if caller workflow fails:
+		{wf["jobs"]["scrape"]["steps"]}
+
 		
-	finally:
-		print("\n\nWhole workflow:\n", wf, "\n\n")
+		'''
+	)
+
+finally:
+	print("\n\nWhole workflow:\n", wf, "\n\n")
 
 
 print("\n\nFile path of failed workflow: " , os.environ["workflow_path"])
